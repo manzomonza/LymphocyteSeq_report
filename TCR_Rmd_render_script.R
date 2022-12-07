30fb12d5f20c3576297b42af32e7653ad77cab46
manzomonza <massimiliano.manzo@posteo.ch>
2022-12-07 15:22:17 +0100

    Update report files

--- TCR_Rmd_render_script.R
+++ TCR_Rmd_render_script.R
@@ -9,10 +9,18 @@ library(optparse)
 
 option_list = list(
   make_option(c("-d", "--dir"), type="character", default=NULL,
-              help="directory with clone summaries", metavar="character"))
+              help="directory with clone summaries", metavar="character"),
+  make_option(c("-f", "--file"), type="character", default=NULL,
+              help="filename of clone summary", metavar="character"))
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser)
 
+filename <- gsub(".clone_summary.csv",'', opt$file)
+filename <- paste0(filename, "_Pan_TCR_report.html")
+
+
 rmarkdown::render('/home/ionadmin/ngs_variant_annotation/variantAnnotation/scripts/TCR_Pan_Clonality.Rmd',
-params = list(filedir =  opt$dir))
+params = list(filedir =  opt$dir),
+output_dir = opt$dir,
+output_file = filename)
