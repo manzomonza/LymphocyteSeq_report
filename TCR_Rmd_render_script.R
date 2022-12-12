c124a2fcf141fb27a761d1d4abe9f24c55e01a49
manzomonza <massimiliano.manzo@posteo.ch>
2022-12-12 16:31:29 +0100

    Modify Render script to accomodate TCR/BCR decision

--- TCR_Rmd_render_script.R
+++ TCR_Rmd_render_script.R
@@ -1,29 +1,23 @@
 #!/usr/bin/R
 
-### Renders TCR_Pan_Clonality.Rmd with specified filedir
+### Decides on Lymphocyte-sequencing panel  
 ################
 ### OPTPARSE ###
 ################
 library(optparse)
 
 option_list = list(
-  make_option(c("-d", "--dir"), type="character", default=NULL,
-              help="directory with clone summaries", metavar="character"),
   make_option(c("-f", "--file"), type="character", default=NULL,
               help="filename of clone summary", metavar="character"))
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser)
 
-filename <- gsub(".clone_summary.csv",'', opt$file)
-sampleID = filename
-sampleID = rev(unlist(stringr::str_split(sampleID, pattern = '/', simplify = FALSE)))[1]
-date_string = gsub("-",'',Sys.Date())
-filename <- paste0(filename, date_string, "_Pan_TCR_report.html")
+## 
 
 
 
-
+## 
 rmarkdown::render('/home/ionadmin/watchdog/TCR_PanClonality/TCR_Pan_Clonality.Rmd',
 params = list(filedir =  opt$dir,
 output_file = filename,
