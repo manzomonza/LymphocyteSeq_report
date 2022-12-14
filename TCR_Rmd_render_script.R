ce5b06c6d32392fdadaa89530b9dc7b0b917a1f2
manzomonza <massimiliano.manzo@posteo.ch>
2022-12-14 10:50:12 +0100

    Edit TCR render to include panel_decision

--- TCR_Rmd_render_script.R
+++ TCR_Rmd_render_script.R
@@ -7,17 +7,15 @@
 library(optparse)
 
 option_list = list(
-  make_option(c("-f", "--file"), type="character", default=NULL,
-              help="filename of clone summary", metavar="character"))
+  make_option(c("-d", "--dir"), type="character", default=NULL,
+              help="directory containing clone summaries", metavar="character"))
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser)
 
 ## 
-
-
-
-## 
+panel_df = panel_dataframe(opt$dir)
+if(panel_decision(panel_df)
 rmarkdown::render('/home/ionadmin/watchdog/TCR_PanClonality/TCR_Pan_Clonality.Rmd',
 params = list(filedir =  opt$dir,
 output_file = filename,
