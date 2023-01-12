6e4a214bc6797516d6a8adeb7980419ce5846876
manzomonza <massimiliano.manzo@posteo.ch>
2023-01-12 13:46:06 +0100

    Add lineage summary functionality

--- LymphocyteSeq_panel_render.R
+++ LymphocyteSeq_panel_render.R
@@ -1,6 +1,6 @@
 #!/usr/bin/R
 
-### Decides on Lymphocyte-sequencing panel  
+### Decides on Lymphocyte-sequencing panel
 ################
 ### OPTPARSE ###
 ################
@@ -13,16 +13,16 @@ tcr_rmarkdown = '/home/ionadmin/watchdog/LymphocyteSeq_report/TCR_report.Rmd'
 bcr_rmarkdown = '/Users/manzo/USB/USB_Diagnostics/LymphocyteSeq_report/BCR_report.Rmd'
 tcr_rmarkdown = '/Users/manzo/USB/USB_Diagnostics/LymphocyteSeq_report/TCR_report.Rmd'
 
-
 option_list = list(
   make_option(c("-d", "--dir"), type="character", default=NULL,
               help="directory containing clone summaries", metavar="character"))
-
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser)
-## List clone summaries
-clonesummaries = only_clonesummaries(opt$dir)
 
+usedir = normalizePath(opt$dir)
+
+## List clone summaries
+clonesummaries = list_clonesummaries(usedir)
 panel_df = panel_dataframe(clonesummaries)
 panel_df = lapply(panel_df, check_panel_dataframe)
 
@@ -32,6 +32,7 @@ for(e in panel_df){
   if(!is.null(e)){
     if(panel_decision(e) == 'BCR'){
       panel_specific_rmarkdown = bcr_rmarkdown
+      lineage_df = panel_dataframe(list_lineage_summaries(usedir))[[1]]
 
     }else if(panel_decision(e) == 'TCR'){
       panel_specific_rmarkdown = tcr_rmarkdown
@@ -42,11 +43,21 @@ for(e in panel_df){
     panel_info = unique(e$panel)
     ## Print both files
     sample_ID = filename_extract(e$filepath[1], e$filepath[2])
-    html_filename = paste0(opt$dir,"/", sample_ID, '_', panel_info, '.html')
+    html_filename = paste0(usedir,"/", sample_ID, '_', panel_info, '.html')
     print(panel_specific_rmarkdown)
+
+    if(panel_decision(e) == 'BCR'){
+    rmarkdown::render(panel_specific_rmarkdown,
+                      params = list(panel_df = e,
+                                    sample_ID = sample_ID,
+                                    lineage_df = lineage_df),
+                                    output_file = html_filename,
+                      )
+  }else if(panel_decision(e) == 'TCR'){
     rmarkdown::render(panel_specific_rmarkdown,
                       params = list(panel_df = e,
                                     sample_ID = sample_ID),
-                                    output_file = html_filename)
+                      output_file = html_filename)
+  }
   }
 }
